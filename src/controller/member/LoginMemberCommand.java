4a184e97a22abc4114e42f32a970caf92f2adf8b
51265 <51265@DESKTOP-M281CPF.mshome.net>
2021-10-26 20:27:27 +0900

    [기능 변경] 로그인 후 메인으로 이동

--- src/controller/member/LoginMemberCommand.java
+++ src/controller/member/LoginMemberCommand.java
@@ -18,32 +18,29 @@ public class LoginMemberCommand implements Command {
 		try {
 			String id = req.getParameter("id");
 			String pwd = req.getParameter("pwd");
-			
-			
+
 			// 3.DB에서 게시글 번호에 해당하는 게시글 정보를 구한다.
 			MemberService service = MemberService.getInstance();
-			
+
 			MemberVo member = service.loginMember(id, pwd);
 			// 4.session 영역에 "member" 속성이름으로 게시글 정보를 저장한다.
-			
+
 			HttpSession session = req.getSession();
-			
-			if(member.getMemNo() == 0) {
-				System.out.println(member.getMemNo());
+
+			if (member.getMemNo() == 0) { //회원이 아닌경우
 				session.setAttribute("message", "아이디 혹은 비밀번호가 일치하지 않습니다.");
-				return new ActionForward("/loginMemberForm.jsp", false);
+				return new ActionForward("/member_login.jsp", true);
 			} else {
-			session.removeAttribute("message");
-			session.setAttribute("member", member);
-			
-			System.out.println(member.getMemNo());
-			
-			return new ActionForward("/loginMemberForm.jsp", false);
+				session.removeAttribute("message");
+				
+				session.setAttribute("member", member);
+
+				return new ActionForward("/index.jsp", true);
 			}
-			
-			} catch (Exception ex) {
-				throw ex;
-			}
+
+		} catch (Exception ex) {
+			throw ex;
+		}
 	}
 
 }
