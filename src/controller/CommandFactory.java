e32885c48901b9d9a9409feebd03db037bf87803
MZC <MZC@DESKTOP-3F8449F>
2021-10-26 10:15:24 +0900

    [기능추가] 쪽지 기능, 통계 기능

--- src/controller/CommandFactory.java
+++ src/controller/CommandFactory.java
@@ -19,6 +19,44 @@ public class CommandFactory {
 		// 게시글 읽기 요청
 		map.put("/detailBoard.do", "controller.DetailBoardCommand");
 		
+		// 멤버 로그 리스트 요청
+		map.put("/MemberLog_List.do", "controller.statistic.ListMemberLogCommand");
+		
+		// 멤버 로그 그래프 폼 요청
+		map.put("/MemberLog_WeeklyGraphForm.do", "controller.statistic.ListMemberLogWeeklyGraphFormCommand");
+		
+		// 멤버 로그 그래프 요청
+		map.put("/MemberLog_WeeklyGraph.do", "controller.statistic.ListMemberLogWeeklyGraphCommand");
+		
+		// 쪽지 쓰기 폼 요청
+		map.put("/Note_WriteNoteForm.do", "controller.note.WriteNoteFormCommand");
+		
+		// 쪽지 쓰기 요청
+		map.put("/Note_WriteNote.do", "controller.note.WriteNoteCommand");
+		
+		// 받은 쪽지 리스트 조회
+		map.put("/Note_ReceiveList.do", "controller.note.ListReceiveNoteCommand");
+		
+		// 보낸 쪽지 리스트 조회
+		map.put("/Note_SendList.do", "controller.note.ListSendNoteCommand");
+		
+		// 보관함 리스트 조회
+		map.put("/Note_SaveList.do", "controller.note.ListSaveNoteCommand");
+		
+		// 쪽지 상세 조회
+		map.put("/Note_DetailNote.do", "controller.note.DetailNoteCommand");
+		
+		// 선택한 쪽지 삭제
+		map.put("/Note_CancelReceiveNote.do", "controller.note.RemoveReceiveNoteCommand");
+		
+		// 보관함 쪽지 삭제
+		map.put("/Note_CancelSaveNote.do", "controller.note.RemoveSaveNoteCommand");
+		
+		// 보관함에 저장
+		map.put("/Note_ModifyNoteSave.do","controller.note.ModifyNoteSaveCommand");
+		
+		// 보관함 쪽지 상세 조회
+		map.put("/Note_DetailSaveNote.do", "controller.note.DetailSaveNoteCommand");
 	}
 
 	// 외부에서 중복된 생성자를 만드는 것을 방지하기 위해서 싱글톤 작성
