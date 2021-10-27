c8dfcd067739a7a14cb62497f1f25eac2e69ecf3
51265 <51265@DESKTOP-M281CPF.mshome.net>
2021-10-27 10:24:54 +0900

    [기능추가] realTime추가

---
+++ src/domain/RealTime.java
@@ -0,0 +1,8 @@
+package domain;
+
+import java.util.ArrayList;
+
+public class RealTime {
+	public static ArrayList<String> memberIds = new ArrayList<String>();
+	public static int memberCount = 0 ;
+}
